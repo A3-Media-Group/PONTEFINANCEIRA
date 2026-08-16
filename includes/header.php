fd743ff4c8478ce35c7cfe5e298f561610144b8e
A3-Media-Group <adm@a3mediagroup.com.br>
2026-08-16 17:48:11 -0300

    Corrige MIME type de downloads e cache-busting do CSS
    - .xlsx/.xls/.pdf agora servidos com content-type correto (estavam
      vindo como text/plain por falta de mapeamento no Apache do host)
    - CSS agora carrega com ?v=<mtime>, evitando que visitantes fiquem
      presos numa versão antiga por até 1 semana (cache do style.css)
      a cada novo deploy

--- includes/header.php
+++ includes/header.php
@@ -51,7 +51,8 @@ if (!isset($body_class))        $body_class = '';
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,600;9..144,700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="/assets/css/style.css">
+<?php $css_v = @filemtime(__DIR__ . '/../assets/css/style.css') ?: '1'; ?>
+<link rel="stylesheet" href="/assets/css/style.css?v=<?php echo $css_v; ?>">
 
 <?php if (!empty($schema_json)): ?>
 <script type="application/ld+json"><?php echo $schema_json; ?></script>
